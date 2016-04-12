e620640cad9ceddd1e4b521ff0284cb1318d5312
Enrico Rubboli <rubboli@gmail.com>
2016-04-12 21:12:29 +0200

    add stats and tests

---
+++ stats.go
@@ -0,0 +1,30 @@
+package bitfinex
+
+import "strings"
+
+type StatsService struct {
+	client *Client
+}
+
+type Stats struct {
+	Period int64
+	Volume float64 `json:"volume,string"`
+}
+
+func (s *StatsService) All(pair string) ([]Stats, error) {
+	pair = strings.ToUpper(pair)
+	req, err := s.client.NewRequest("GET", "stats/"+pair)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var stats []Stats
+	_, err = s.client.Do(req, &stats)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return stats, nil
+}
