a8aa14123f15e243ca0ef22e0dfd75f09b7b1114
Sascha Hanse <shanse@posteo.de>
2017-03-27 02:41:14 +0200

    revert the change of using a random number for the nonce because the nonce MUST BE increasing.

--- utils/nonce.go
+++ utils/nonce.go
@@ -1,19 +1,22 @@
 package utils
 
 import (
-	"crypto/rand"
-	"math"
-	"math/big"
+	"strconv"
+	"sync/atomic"
 	"time"
 )
 
-// GetNonce produces a random integer >= time.Now().UnixNano().
-func GetNonce() (string, error) {
-	t := big.NewInt(time.Now().UnixNano())
-	bi, err := rand.Int(rand.Reader, t.Add(t, big.NewInt(math.MaxInt64)))
-	if err != nil {
-		return "", err
-	}
+var nonce uint64
 
-	return bi.String(), nil
+func init() {
+	nonce = uint64(time.Now().UnixNano())
+}
+
+// GetNonce is a naive nonce producer that takes the current Unix nano epoch
+// and counts upwards.
+// This is a naive approach because the nonce bound to the currently used API
+// key and as such needs to be synchronised with other instances using the same
+// key in order to avoid race conditions.
+func GetNonce() string {
+	return strconv.FormatUint(atomic.AddUint64(&nonce, 1), 10)
 }
