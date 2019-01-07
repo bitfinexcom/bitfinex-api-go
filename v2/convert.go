9135870d172475f7ab6d887091d8b537219f67ab
Jacob Plaster <jacob.plaster@youview.com>
2019-01-07 14:01:15 +0000

    lint-fix: remove unused funcs

--- v2/convert.go
+++ v2/convert.go
@@ -31,40 +31,11 @@ func iValOrZero(i interface{}) int {
 	return 0
 }
 
-func i64pValOrNil(i interface{}) *int64 {
-	if i == nil {
-		return nil
-	}
-
-	if r, ok := i.(int64); ok {
-		return &r
-	}
-	return nil
-}
-
-func ui64ValOrZero(i interface{}) uint64 {
-	if r, ok := i.(float64); ok {
-		return uint64(r)
-	}
-	return 0
-}
-
 func f64ValOrZero(i interface{}) float64 {
 	if r, ok := i.(float64); ok {
 		return r
 	}
 	return 0.0
-}
-
-func f64pValOrNil(i interface{}) *float64 {
-	if i == nil {
-		return nil
-	}
-
-	if r, ok := i.(float64); ok {
-		return &r
-	}
-	return nil
 }
 
 func bValOrFalse(i interface{}) bool {
