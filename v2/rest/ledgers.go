17bfc7a81bbe729b8075ace86ca12fff2e4261a1
remco-remco <49029137+remco-remco@users.noreply.github.com>
2019-03-28 17:33:11 +0100

    Update ledgers.go

--- v2/rest/ledgers.go
+++ v2/rest/ledgers.go
@@ -12,8 +12,8 @@ type LedgerService struct {
 }
 
 // returns the ledgers from the selected currency for authenticated account.
-func (s *LedgerService) All(symbol string) (*bitfinex.LedgerSnapshot, error) {
-	req, err := s.requestFactory.NewAuthenticatedRequestWithData(path.Join("ledgers", symbol, "hist"), map[string]interface{}{"start": nil, "end": nil, "limit": nil})
+func (s *LedgerService) Ledger(currency string, start int32, end int32, limit int32) (string, error) {
+	req, err := s.requestFactory.NewAuthenticatedRequestWithData(path.Join("ledgers", currency, "hist"), map[string]interface{}{"start": start, "end": end, "limit": limit})
 	if err != nil {
 		return nil, err
 	}
@@ -22,11 +22,6 @@ func (s *LedgerService) All(symbol string) (*bitfinex.LedgerSnapshot, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	os, err := bitfinex.NewLedgerSnapshotFromRaw(raw)
-	if err != nil {
-		return nil, err
-	}
 	
-	return os, nil
+	return raw, nil
 }
