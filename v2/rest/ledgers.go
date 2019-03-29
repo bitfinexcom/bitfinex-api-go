a986bb61cd533ef1a1d5f49d65475290fbf7f3aa
remco-remco <49029137+remco-remco@users.noreply.github.com>
2019-03-29 01:12:52 +0100

    Create ledgers.go

---
+++ v2/rest/ledgers.go
@@ -0,0 +1,38 @@
+package rest
+
+import (
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
+	"path"
+)
+
+// LedgerService manages the Ledgers endpoint.
+type LedgerService struct {
+	requestFactory
+	Synchronous
+}
+
+// Ledgers returns ledgers for the selected currency and authenticated account.
+func (s *LedgerService) Ledgers(symbol string) (*bitfinex.LedgerSnapshot, error) {
+	req, err := s.requestFactory.NewAuthenticatedRequestWithData(path.Join("ledgers", symbol, "hist"), map[string]interface{}{"start": nil, "end": nil, "limit": nil})
+	if err != nil {
+		return nil, err
+	}
+	raw, err := s.Request(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	dat := make([][]float64, 0)
+	for _, r := range raw {
+		if f, ok := r.([]float64); ok {
+			dat = append(dat, f)
+		}
+	}
+
+	os, err := bitfinex.NewLedgerSnapshotFromRaw(symbol, dat)
+	if err != nil {
+		return nil, err
+	}
+	return os, nil
+}
