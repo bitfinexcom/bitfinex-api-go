d27bb3b6ff6b7d64fc67da11f7a5b2e6a7d64d5f
remco-remco <49029137+remco-remco@users.noreply.github.com>
2019-03-29 01:39:45 +0100

    Create currencys.go

---
+++ v2/currencys.go
@@ -0,0 +1,17 @@
+package bitfinex
+
+// Available currencys
+const (
+	BTC = "BTC"
+	LTC = "LTC"
+	ETH = "ETH"
+	ETC = "ETC"
+	BFX = "BFX"
+	ZEC = "ZEC"
+	XMR = "XMR"
+	RRT = "RRT"
+	XRP = "XRP"
+	EOS = "EOS"
+	IOT = "IOT"
+	BCC = "BCC"
+)
