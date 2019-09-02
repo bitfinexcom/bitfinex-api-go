819d710369f5c4447718667708cb78dc649d0506
Jacob Plaster <jacob.plaster@youview.com>
2019-09-02 15:30:27 +0100

    examples: add rest wallet example

--- examples/v2/rest-wallet/main.go
+++ examples/v2/rest-wallet/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-"flag"
-"log"
-"os"
-"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
+	"fmt"
+	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
 	"github.com/davecgh/go-spew/spew"
+	"log"
+	"os"
 )
 
 
@@ -17,22 +17,25 @@ import (
 // you can obtain it from https://www.bitfinex.com/api
 
 func main() {
-	flag.Parse()
-
-	key := os.Getenv("BFX_API_KEY")
-	secret := os.Getenv("BFX_API_SECRET")
-	c := rest.NewClient().Credentials(key, secret)
+	key := os.Getenv("BFX_KEY")
+	secret := os.Getenv("BFX_SECRET")
+	c := rest.NewClientWithURL("https://test.bitfinex.com/v2/").Credentials(key, secret)
 
 	wallets, err := c.Wallet.Wallet()
-
-
 	if err != nil {
 		log.Fatalf("getting wallet %s", err)
 	}
-
 	spew.Dump(wallets)
 
+	notfication, err := c.Wallet.Transfer("exchange", "margin", "BTC", "BTC", 0.1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(notfication)
 
-
+	notfication2, err := c.Wallet.DepositAddress("exchange", "bitcoin")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(notfication2)
 }
-
