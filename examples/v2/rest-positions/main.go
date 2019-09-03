8089d745c018cc70850d869de9a23d0e31bad69e
Jacob Plaster <jacob.plaster@youview.com>
2019-09-03 09:47:49 +0100

    examples: add claim position example

--- examples/v2/rest-positions/main.go
+++ examples/v2/rest-positions/main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
-	"github.com/davecgh/go-spew/spew"
+	"log"
+	"os"
 )
 
 
 func main() {
-	apikey := ""
-	secKey := ""
+	key := os.Getenv("BFX_KEY")
+	secret := os.Getenv("BFX_SECRET")
+	uri := "https://api.bitfinex.com/v2/"
 
-	c := rest.NewClient().Credentials(apikey, secKey)
-
+	c := rest.NewClientWithURL(uri).Credentials(key, secret)
+	// get active positions
 	positions, err := c.Positions.All()
-
 	if err != nil {
 		log.Fatalf("getting wallet %s", err)
 	}
-
-	spew.Dump(positions)
+	for _, p := range positions.Snapshot {
+		fmt.Println(p)
+	}
+	// claim active position
+	pClaim, err := c.Positions.Claim(&bitfinex.ClaimPositionRequest{
+		Id: 36228736,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pClaim.NotifyInfo.(*bitfinex.PositionCancel))
 }
 
