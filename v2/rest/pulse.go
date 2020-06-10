e6fe773478dbb7dd840c96f02e22112f07597b0e
Daniel1984 <aciddaniel@gmail.com>
2020-06-10 19:31:41 +0300

    new PublicPulseHistory restv2 method

--- v2/rest/pulse.go
+++ v2/rest/pulse.go
@@ -2,8 +2,10 @@ package rest
 
 import (
 	"fmt"
+	"net/url"
 	"path"
 
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/pulse"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/pulseprofile"
 )
 
@@ -12,13 +14,13 @@ type PulseService struct {
 	Synchronous
 }
 
-func (ss *PulseService) PublicPulseProfile(nickname string) (*pulseprofile.PulseProfile, error) {
+func (ps *PulseService) PublicPulseProfile(nickname string) (*pulseprofile.PulseProfile, error) {
 	if (len(nickname)) == 0 {
 		return nil, fmt.Errorf("nickname is required argument")
 	}
 
 	req := NewRequestWithMethod(path.Join("pulse", "profile", nickname), "GET")
-	raw, err := ss.Request(req)
+	raw, err := ps.Request(req)
 	if err != nil {
 		return nil, err
 	}
@@ -29,4 +31,23 @@ func (ss *PulseService) PublicPulseProfile(nickname string) (*pulseprofile.Pulse
 	}
 
 	return pp, nil
+}
+
+func (ps *PulseService) PublicPulseHistory(limit, end string) ([]*pulse.Pulse, error) {
+	req := NewRequestWithMethod(path.Join("pulse", "hist"), "GET")
+	req.Params = make(url.Values)
+	req.Params.Add("limit", limit)
+	req.Params.Add("end", end)
+
+	raw, err := ps.Request(req)
+	if err != nil {
+		return nil, err
+	}
+
+	pph, err := pulse.NewFromRaw(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	return pph, nil
 }
