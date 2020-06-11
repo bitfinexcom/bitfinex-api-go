12f439d6c0b1bc62f4caf6de24522a1869c5ce65
Daniel1984 <aciddaniel@gmail.com>
2020-06-11 17:18:46 +0300

    RESTv2 new AddPulse function

--- v2/rest/pulse.go
+++ v2/rest/pulse.go
@@ -1,12 +1,14 @@
 package rest
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"path"
 
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/pulse"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/pulseprofile"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 )
 
 type PulseService struct {
@@ -14,6 +16,7 @@ type PulseService struct {
 	Synchronous
 }
 
+// PublicPulseProfile get pulse profile by nickname
 func (ps *PulseService) PublicPulseProfile(nickname string) (*pulseprofile.PulseProfile, error) {
 	if (len(nickname)) == 0 {
 		return nil, fmt.Errorf("nickname is required argument")
@@ -33,6 +36,7 @@ func (ps *PulseService) PublicPulseProfile(nickname string) (*pulseprofile.Pulse
 	return pp, nil
 }
 
+// PublicPulseHistory returns public pulse messages
 func (ps *PulseService) PublicPulseHistory(limit, end string) ([]*pulse.Pulse, error) {
 	req := NewRequestWithMethod(path.Join("pulse", "hist"), "GET")
 	req.Params = make(url.Values)
@@ -50,4 +54,34 @@ func (ps *PulseService) PublicPulseHistory(limit, end string) ([]*pulse.Pulse, e
 	}
 
 	return pph, nil
+}
+
+// AddPulse submits pulse messages
+func (ps *PulseService) AddPulse(p *pulse.Pulse) (*pulse.Pulse, error) {
+	tl := len(p.Title)
+	if tl < 16 || tl > 120 {
+		return nil, fmt.Errorf("Title length min 16 and max 120 characters. Got:%d", tl)
+	}
+
+	payload, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := ps.requestFactory.NewAuthenticatedRequestWithBytes(bitfinex.PermissionWrite, path.Join("pulse", "add"), payload)
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := ps.Request(req)
+	if err != nil {
+		return nil, err
+	}
+
+	pm, err := pulse.NewSingleFromRaw(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	return pm, nil
 }
