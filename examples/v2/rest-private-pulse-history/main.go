4860151b0c0d4db0f8982836e7c9815a80c3be0c
Daniel1984 <aciddaniel@gmail.com>
2020-06-12 13:49:20 +0300

    new PulseHistory function usage example

---
+++ examples/v2/rest-private-pulse-history/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
+	"github.com/davecgh/go-spew/spew"
+)
+
+// Set BFX_API_KEY and BFX_API_SECRET:
+//
+// export BFX_API_KEY=<your-api-key>
+// export BFX_API_SECRET=<your-api-secret>
+//
+// you can obtain it from https://www.bitfinex.com/api
+
+func main() {
+	key := os.Getenv("BFX_API_KEY")
+	secret := os.Getenv("BFX_API_SECRET")
+
+	c := rest.
+		NewClientWithURL("https://api.staging.bitfinex.com/v2/").
+		Credentials(key, secret)
+
+	pulseHist, err := c.Pulse.PulseHistory(1)
+	if err != nil {
+		log.Fatalf("PulseHistory: %s", err)
+	}
+
+	spew.Dump(pulseHist)
+}
