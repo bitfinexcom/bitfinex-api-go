ddb1fa945c43a25dcaf2c3e47f123899786b0c9b
Daniel1984 <aciddaniel@gmail.com>
2020-06-29 15:16:35 +0300

    new OrderMultiOp function usage example

--- examples/v2/rest-order-multi/main.go
+++ examples/v2/rest-order-multi/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
 	"github.com/davecgh/go-spew/spew"
 )
@@ -26,12 +27,54 @@ func main() {
 		Credentials(key, secret)
 
 	cancelOrdersByID(c)
+	orderMultiOp(c)
 }
 
 func cancelOrdersByID(c *rest.Client) {
 	resp, err := c.Orders.CancelOrders(rest.OrderIDs{1189452506, 1189452507})
 	if err != nil {
 		log.Fatalf("cancelAllOrders error: %s", err)
+	}
+
+	spew.Dump(resp)
+}
+
+func orderMultiOp(c *rest.Client) {
+	pld := rest.OrderMultiArgs{
+		Ops: [][]interface{}{
+			{
+				"on",
+				bitfinex.OrderNewRequest{
+					CID:    987,
+					GID:    876,
+					Type:   "EXCHANGE LIMIT",
+					Symbol: "tBTCUSD",
+					Price:  13,
+					Amount: 0.001,
+				},
+			},
+			{
+				"oc",
+				map[string]int{"id": 1189502430},
+			},
+			{
+				"oc_multi",
+				map[string][]int{"id": rest.OrderIDs{1189502431, 1189502432}},
+			},
+			{
+				"ou",
+				bitfinex.OrderUpdateRequest{
+					ID:     1189502433,
+					Price:  15,
+					Amount: 0.002,
+				},
+			},
+		},
+	}
+
+	resp, err := c.Orders.OrderMultiOp(pld)
+	if err != nil {
+		log.Fatalf("OrderMultiOp error: %s", err)
 	}
 
 	spew.Dump(resp)
