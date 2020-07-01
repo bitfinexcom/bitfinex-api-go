0dc49922314d59a94163d45e0bb27d08ea7e1a84
Daniel1984 <aciddaniel@gmail.com>
2020-07-01 14:46:21 +0300

    v2/rest/market.go new ForeignExchangeRate function with test coverage

--- v2/rest/market_test.go
+++ v2/rest/market_test.go
@@ -46,3 +46,49 @@ func TestMarketAveragePrice(t *testing.T) {
 		assert.Equal(t, expected, avgPrice)
 	})
 }
+
+func TestForeignExchangeRate(t *testing.T) {
+	t.Run("missing arguments", func(t *testing.T) {
+		c := rest.NewClient()
+		rsp, err := c.Market.ForeignExchangeRate(rest.ForeignExchangeRateRequest{
+			FirstCurrency: "BTC",
+		})
+
+		require.NotNil(t, err)
+		require.Nil(t, rsp)
+	})
+
+	t.Run("calls correct resource with correct payload", func(t *testing.T) {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "/calc/fx", r.RequestURI)
+			assert.Equal(t, "POST", r.Method)
+
+			gotReqPld := rest.ForeignExchangeRateRequest{}
+			err := json.NewDecoder(r.Body).Decode(&gotReqPld)
+			require.Nil(t, err)
+
+			expectedReqPld := rest.ForeignExchangeRateRequest{
+				FirstCurrency:  "BTC",
+				SecondCurrency: "USD",
+			}
+			assert.Equal(t, expectedReqPld, gotReqPld)
+
+			respMock := []interface{}{9151.5}
+			payload, _ := json.Marshal(respMock)
+			_, err = w.Write(payload)
+			require.Nil(t, err)
+		}
+
+		server := httptest.NewServer(http.HandlerFunc(handler))
+		defer server.Close()
+
+		c := rest.NewClientWithURL(server.URL)
+		rsp, err := c.Market.ForeignExchangeRate(rest.ForeignExchangeRateRequest{
+			FirstCurrency:  "BTC",
+			SecondCurrency: "USD",
+		})
+
+		require.Nil(t, err)
+		assert.Equal(t, float64(9151.5), rsp[0])
+	})
+}
