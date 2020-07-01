7ae829b6d139bd147defbda241e98d8e7178812a
Daniel1984 <aciddaniel@gmail.com>
2020-07-01 12:14:28 +0300

    OrderMultiOp - enrich order update/create test coverage

--- v2/rest/orders_test.go
+++ v2/rest/orders_test.go
@@ -277,9 +277,58 @@ func TestOrderMultiOp(t *testing.T) {
 			assert.Equal(t, "/auth/w/order/multi", r.RequestURI)
 			assert.Equal(t, "POST", r.Method)
 
+			gotReqPld := OrderMultiOpsRequest{}
+			err := json.NewDecoder(r.Body).Decode(&gotReqPld)
+			require.Nil(t, err)
+
+			expectedReqPld := map[string][]interface{}{
+				"on": {
+					"on",
+					map[string]interface{}{
+						"amount": "0.001",
+						"cid":    float64(987),
+						"flags":  float64(4096),
+						"gid":    float64(876),
+						"meta": map[string]interface{}{
+							"aff_code": "abc",
+						},
+						"price":  "13",
+						"symbol": "tBTCUSD",
+						"type":   "EXCHANGE LIMIT",
+					},
+				},
+				"ou": {
+					"ou",
+					map[string]interface{}{
+						"amount": "0.002",
+						"price":  "15",
+						"id":     float64(1189503342),
+						"flags":  float64(64),
+					},
+				},
+				"oc": {
+					"oc",
+					map[string]interface{}{"id": float64(1189502430)},
+				},
+				"oc_multi": {
+					"oc_multi",
+					map[string]interface{}{
+						"id": []interface{}{
+							float64(1189502431),
+							float64(1189502432),
+						},
+					},
+				},
+			}
+
+			for _, v := range gotReqPld.Ops {
+				key := v[0].(string)
+				assert.Equal(t, expectedReqPld[key][1], v[1])
+			}
+
 			respMock := []interface{}{1568711312683, nil, nil, nil, nil, nil, nil, nil}
 			payload, _ := json.Marshal(respMock)
-			_, err := w.Write(payload)
+			_, err = w.Write(payload)
 			require.Nil(t, err)
 		}
 
@@ -289,12 +338,34 @@ func TestOrderMultiOp(t *testing.T) {
 		c := NewClientWithURL(server.URL)
 		pld := OrderOps{
 			{
+				"on",
+				bitfinex.OrderNewRequest{
+					CID:           987,
+					GID:           876,
+					Type:          "EXCHANGE LIMIT",
+					Symbol:        "tBTCUSD",
+					Price:         13,
+					Amount:        0.001,
+					PostOnly:      true,
+					AffiliateCode: "abc",
+				},
+			},
+			{
 				"oc",
 				map[string]int{"id": 1189502430},
 			},
 			{
 				"oc_multi",
 				map[string][]int{"id": OrderIDs{1189502431, 1189502432}},
+			},
+			{
+				"ou",
+				bitfinex.OrderUpdateRequest{
+					ID:     1189503342,
+					Price:  15,
+					Amount: 0.002,
+					Hidden: true,
+				},
 			},
 		}
 		rsp, err := c.Orders.OrderMultiOp(pld)
