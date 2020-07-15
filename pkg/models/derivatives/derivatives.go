511e552859f1787b51e05d80b3ec28462fa754cc
Daniel1984 <aciddaniel@gmail.com>
2020-07-15 18:06:03 +0300

    safer response payload check for status derivatives

--- pkg/models/derivatives/derivatives.go
+++ pkg/models/derivatives/derivatives.go
@@ -10,12 +10,6 @@ type DerivativeStatusSnapshot struct {
 	Snapshot []*DerivativeStatus
 }
 
-type StatusType string
-
-const (
-	DerivativeStatusType StatusType = "deriv"
-)
-
 type DerivativeStatus struct {
 	Symbol               string
 	MTS                  int64
@@ -31,62 +25,63 @@ type DerivativeStatus struct {
 }
 
 func NewDerivativeStatusFromWsRaw(symbol string, raw []interface{}) (*DerivativeStatus, error) {
-	if len(raw) == 18 {
-		ds := &DerivativeStatus{
-			Symbol: symbol,
-			MTS:    convert.I64ValOrZero(raw[0]),
-			// placeholder
-			Price:     convert.F64ValOrZero(raw[2]),
-			SpotPrice: convert.F64ValOrZero(raw[3]),
-			// placeholder
-			InsuranceFundBalance: convert.F64ValOrZero(raw[5]),
-			// placeholder
-			FundingEventMTS: convert.I64ValOrZero(raw[7]),
-			FundingAccrued:  convert.F64ValOrZero(raw[8]),
-			FundingStep:     convert.F64ValOrZero(raw[9]),
-			// placeholder
-			CurrentFunding: convert.F64ValOrZero(raw[11]),
-			// placeholder
-			// placeholder
-			MarkPrice: convert.F64ValOrZero(raw[14]),
-			// placeholder
-			// placeholder
-			OpenInterest: convert.F64ValOrZero(raw[17]),
-		}
-		return ds, nil
+	if len(raw) < 18 {
+		return nil, fmt.Errorf("unexpected data slice length for derivative status: %#v", raw)
 	}
 
-	return nil, fmt.Errorf("unexpected data slice length for derivative status: %#v", raw)
+	ds := &DerivativeStatus{
+		Symbol: symbol,
+		MTS:    convert.I64ValOrZero(raw[0]),
+		// placeholder
+		Price:     convert.F64ValOrZero(raw[2]),
+		SpotPrice: convert.F64ValOrZero(raw[3]),
+		// placeholder
+		InsuranceFundBalance: convert.F64ValOrZero(raw[5]),
+		// placeholder
+		FundingEventMTS: convert.I64ValOrZero(raw[7]),
+		FundingAccrued:  convert.F64ValOrZero(raw[8]),
+		FundingStep:     convert.F64ValOrZero(raw[9]),
+		// placeholder
+		CurrentFunding: convert.F64ValOrZero(raw[11]),
+		// placeholder
+		// placeholder
+		MarkPrice: convert.F64ValOrZero(raw[14]),
+		// placeholder
+		// placeholder
+		OpenInterest: convert.F64ValOrZero(raw[17]),
+	}
+
+	return ds, nil
 }
 
 func NewDerivativeStatusFromRaw(raw []interface{}) (*DerivativeStatus, error) {
-	if len(raw) == 19 {
-		ds := &DerivativeStatus{
-			Symbol: convert.SValOrEmpty(raw[0]),
-			MTS:    convert.I64ValOrZero(raw[1]),
-			// placeholder
-			Price:     convert.F64ValOrZero(raw[3]),
-			SpotPrice: convert.F64ValOrZero(raw[4]),
-			// placeholder
-			InsuranceFundBalance: convert.F64ValOrZero(raw[6]),
-			// placeholder
-			FundingEventMTS: convert.I64ValOrZero(raw[8]),
-			FundingAccrued:  convert.F64ValOrZero(raw[9]),
-			FundingStep:     convert.F64ValOrZero(raw[10]),
-			// placeholder
-			CurrentFunding: convert.F64ValOrZero(raw[12]),
-			// placeholder
-			// placeholder
-			MarkPrice: convert.F64ValOrZero(raw[15]),
-			// placeholder
-			// placeholder
-			OpenInterest: convert.F64ValOrZero(raw[18]),
-		}
-
-		return ds, nil
+	if len(raw) < 19 {
+		return nil, fmt.Errorf("unexpected data slice length for derivative status: %#v", raw)
 	}
 
-	return nil, fmt.Errorf("unexpected data slice length for derivative status: %#v", raw)
+	ds := &DerivativeStatus{
+		Symbol: convert.SValOrEmpty(raw[0]),
+		MTS:    convert.I64ValOrZero(raw[1]),
+		// placeholder
+		Price:     convert.F64ValOrZero(raw[3]),
+		SpotPrice: convert.F64ValOrZero(raw[4]),
+		// placeholder
+		InsuranceFundBalance: convert.F64ValOrZero(raw[6]),
+		// placeholder
+		FundingEventMTS: convert.I64ValOrZero(raw[8]),
+		FundingAccrued:  convert.F64ValOrZero(raw[9]),
+		FundingStep:     convert.F64ValOrZero(raw[10]),
+		// placeholder
+		CurrentFunding: convert.F64ValOrZero(raw[12]),
+		// placeholder
+		// placeholder
+		MarkPrice: convert.F64ValOrZero(raw[15]),
+		// placeholder
+		// placeholder
+		OpenInterest: convert.F64ValOrZero(raw[18]),
+	}
+
+	return ds, nil
 }
 
 func NewDerivativeSnapshotFromRaw(raw [][]interface{}) (*DerivativeStatusSnapshot, error) {
