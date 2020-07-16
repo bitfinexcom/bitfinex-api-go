e97eaf946dfc7dffa79179b9d18d4eaf54762ae3
Daniel1984 <aciddaniel@gmail.com>
2020-07-16 17:31:22 +0300

    v2/rest/candles.go refactor, simplify, test, repeat... :)

--- v2/rest/candles.go
+++ v2/rest/candles.go
@@ -1,12 +1,15 @@
 package rest
 
 import (
-	"github.com/bitfinexcom/bitfinex-api-go/v2"
+	"fmt"
+	"net/url"
 	"path"
 	"strconv"
 	"strings"
-	"net/url"
-	"fmt"
+
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/convert"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/candle"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 )
 
 // CandleService manages the Candles endpoint.
@@ -14,25 +17,32 @@ type CandleService struct {
 	Synchronous
 }
 
-// Retrieve the last candle for the given symbol with the given resolution
-// See https://docs.bitfinex.com/reference#rest-public-candles for more info
-func (c *CandleService) Last(symbol string, resolution bitfinex.CandleResolution) (*bitfinex.Candle, error) {
-	if symbol == "" {
-		return nil, fmt.Errorf("symbol cannot be empty")
+func getPathSegments(symbol string, resolution bitfinex.CandleResolution) (s string, err error) {
+	if len(symbol) == 0 {
+		err = fmt.Errorf("symbol cannot be empty")
+		return
 	}
 
-	segments := []string{ "trade", string(resolution), symbol }
+	segments := []string{"trade", string(resolution), symbol}
+	s = strings.Join(segments, ":")
+	return
+}
 
-	req := NewRequestWithMethod(path.Join("candles", strings.Join(segments,":"), "LAST"), "GET")
-	req.Params = make(url.Values)
-	raw, err := c.Request(req)
-
+// Last - retrieve the last candle for the given symbol with the given resolution
+// See https://docs.bitfinex.com/reference#rest-public-candles for more info
+func (c *CandleService) Last(symbol string, resolution bitfinex.CandleResolution) (*candle.Candle, error) {
+	segments, err := getPathSegments(symbol, resolution)
 	if err != nil {
 		return nil, err
 	}
 
-	cs, err := bitfinex.NewCandleFromRaw(symbol, resolution, raw)
+	req := NewRequestWithMethod(path.Join("candles", segments, "LAST"), "GET")
+	raw, err := c.Request(req)
+	if err != nil {
+		return nil, err
+	}
 
+	cs, err := candle.FromRaw(symbol, resolution, raw)
 	if err != nil {
 		return nil, err
 	}
@@ -40,38 +50,21 @@ func (c *CandleService) Last(symbol string, resolution bitfinex.CandleResolution
 	return cs, nil
 }
 
-// Retrieves all candles (Max=1000) with the given symbol and the given candle resolution
+// History - retrieves all candles (Max=1000) with the given symbol and the given candle resolution
 // See https://docs.bitfinex.com/reference#rest-public-candles for more info
-func (c *CandleService) History(symbol string, resolution bitfinex.CandleResolution) (*bitfinex.CandleSnapshot, error) {
-	if symbol == "" {
-		return nil, fmt.Errorf("symbol cannot be empty")
-	}
-
-	segments := []string{ "trade", string(resolution), symbol }
-
-	req := NewRequestWithMethod(path.Join("candles", strings.Join(segments,":"), "HIST"), "GET")
-
-	raw, err := c.Request(req)
-
+func (c *CandleService) History(symbol string, resolution bitfinex.CandleResolution) (*candle.Snapshot, error) {
+	segments, err := getPathSegments(symbol, resolution)
 	if err != nil {
 		return nil, err
 	}
 
-	data := make([][]float64, 0, len(raw))
-	for _, ifacearr := range raw {
-		if arr, ok := ifacearr.([]interface{}); ok {
-			sub := make([]float64, 0, len(arr))
-			for _, iface := range arr {
-				if flt, ok := iface.(float64); ok {
-					sub = append(sub, flt)
-				}
-			}
-			data = append(data, sub)
-		}
+	req := NewRequestWithMethod(path.Join("candles", segments, "HIST"), "GET")
+	raw, err := c.Request(req)
+	if err != nil {
+		return nil, err
 	}
 
-	cs, err := bitfinex.NewCandleSnapshotFromRaw(symbol, resolution, data)
-
+	cs, err := candle.SnapshotFromRaw(symbol, resolution, convert.ToInterfaceArray(raw))
 	if err != nil {
 		return nil, err
 	}
@@ -79,8 +72,7 @@ func (c *CandleService) History(symbol string, resolution bitfinex.CandleResolut
 	return cs, nil
 }
 
-
-// Retrieves all candles (Max=1000) that fit the given query criteria
+// HistoryWithQuery - retrieves all candles (Max=1000) that fit the given query criteria
 // See https://docs.bitfinex.com/reference#rest-public-candles for more info
 func (c *CandleService) HistoryWithQuery(
 	symbol string,
@@ -89,42 +81,25 @@ func (c *CandleService) HistoryWithQuery(
 	end bitfinex.Mts,
 	limit bitfinex.QueryLimit,
 	sort bitfinex.SortOrder,
-	) (*bitfinex.CandleSnapshot, error) {
-		if symbol == "" {
-		return nil, fmt.Errorf("symbol cannot be empty")
+) (*candle.Snapshot, error) {
+	segments, err := getPathSegments(symbol, resolution)
+	if err != nil {
+		return nil, err
 	}
 
-	segments := []string{ "trade", string(resolution), symbol }
-
-	req := NewRequestWithMethod(path.Join("candles", strings.Join(segments,":"), "HIST"), "GET")
+	req := NewRequestWithMethod(path.Join("candles", segments, "HIST"), "GET")
 	req.Params = make(url.Values)
-
 	req.Params.Add("end", strconv.FormatInt(int64(end), 10))
 	req.Params.Add("start", strconv.FormatInt(int64(start), 10))
 	req.Params.Add("limit", strconv.FormatInt(int64(limit), 10))
 	req.Params.Add("sort", strconv.FormatInt(int64(sort), 10))
 
 	raw, err := c.Request(req)
-
 	if err != nil {
 		return nil, err
 	}
 
-	data := make([][]float64, 0, len(raw))
-	for _, ifacearr := range raw {
-		if arr, ok := ifacearr.([]interface{}); ok {
-			sub := make([]float64, 0, len(arr))
-			for _, iface := range arr {
-				if flt, ok := iface.(float64); ok {
-					sub = append(sub, flt)
-				}
-			}
-			data = append(data, sub)
-		}
-	}
-
-	cs, err := bitfinex.NewCandleSnapshotFromRaw(symbol, resolution, data)
-
+	cs, err := candle.SnapshotFromRaw(symbol, resolution, convert.ToInterfaceArray(raw))
 	if err != nil {
 		return nil, err
 	}
