c58b19e40a86b6803bd291b3024007e785b3b976
Daniel1984 <aciddaniel@gmail.com>
2020-07-16 17:33:20 +0300

    v2/types.go removing old candle reference

--- v2/types.go
+++ v2/types.go
@@ -1684,54 +1684,6 @@ func NewBookUpdateFromRaw(symbol, precision string, data []interface{}, raw_numb
 	return
 }
 
-type Candle struct {
-	Symbol     string
-	Resolution CandleResolution
-	MTS        int64
-	Open       float64
-	Close      float64
-	High       float64
-	Low        float64
-	Volume     float64
-}
-
-type CandleSnapshot struct {
-	Snapshot []*Candle
-}
-
-func NewCandleSnapshotFromRaw(symbol string, resolution CandleResolution, raw [][]float64) (*CandleSnapshot, error) {
-	if len(raw) <= 0 {
-		return nil, fmt.Errorf("data slice too short for candle snapshot: %#v", raw)
-	}
-	snap := make([]*Candle, 0)
-	for _, f := range raw {
-		c, err := NewCandleFromRaw(symbol, resolution, convert.ToInterface(f))
-		if err == nil {
-			snap = append(snap, c)
-		}
-	}
-	return &CandleSnapshot{Snapshot: snap}, nil
-}
-
-func NewCandleFromRaw(symbol string, resolution CandleResolution, raw []interface{}) (c *Candle, err error) {
-	if len(raw) < 6 {
-		return c, fmt.Errorf("data slice too short for candle, expected %d got %d: %#v", 6, len(raw), raw)
-	}
-
-	c = &Candle{
-		Symbol:     symbol,
-		Resolution: resolution,
-		MTS:        convert.I64ValOrZero(raw[0]),
-		Open:       convert.F64ValOrZero(raw[1]),
-		Close:      convert.F64ValOrZero(raw[2]),
-		High:       convert.F64ValOrZero(raw[3]),
-		Low:        convert.F64ValOrZero(raw[4]),
-		Volume:     convert.F64ValOrZero(raw[5]),
-	}
-
-	return
-}
-
 type Ledger struct {
 	ID          int64
 	Currency    string
