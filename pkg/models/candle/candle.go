744c349a72de249445597bedce0c9d0ab7dc28ac
Daniel1984 <aciddaniel@gmail.com>
2020-07-16 17:28:03 +0300

    separating candle type into separate module

---
+++ pkg/models/candle/candle.go
@@ -0,0 +1,56 @@
+package candle
+
+import (
+	"fmt"
+
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/convert"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
+)
+
+type Candle struct {
+	Symbol     string
+	Resolution bitfinex.CandleResolution
+	MTS        int64
+	Open       float64
+	Close      float64
+	High       float64
+	Low        float64
+	Volume     float64
+}
+
+type Snapshot struct {
+	Snapshot []*Candle
+}
+
+func SnapshotFromRaw(symbol string, resolution bitfinex.CandleResolution, raw [][]interface{}) (*Snapshot, error) {
+	if len(raw) <= 0 {
+		return nil, fmt.Errorf("data slice too short for candle snapshot: %#v", raw)
+	}
+	snap := make([]*Candle, 0)
+	for _, f := range raw {
+		c, err := FromRaw(symbol, resolution, f)
+		if err == nil {
+			snap = append(snap, c)
+		}
+	}
+	return &Snapshot{Snapshot: snap}, nil
+}
+
+func FromRaw(symbol string, resolution bitfinex.CandleResolution, raw []interface{}) (c *Candle, err error) {
+	if len(raw) < 6 {
+		return c, fmt.Errorf("data slice too short for candle, expected %d got %d: %#v", 6, len(raw), raw)
+	}
+
+	c = &Candle{
+		Symbol:     symbol,
+		Resolution: resolution,
+		MTS:        convert.I64ValOrZero(raw[0]),
+		Open:       convert.F64ValOrZero(raw[1]),
+		Close:      convert.F64ValOrZero(raw[2]),
+		High:       convert.F64ValOrZero(raw[3]),
+		Low:        convert.F64ValOrZero(raw[4]),
+		Volume:     convert.F64ValOrZero(raw[5]),
+	}
+
+	return
+}
