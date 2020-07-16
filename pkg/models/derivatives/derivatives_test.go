6ed5536c21f660a9758dfba2e6f702ee90dea278
Daniel1984 <aciddaniel@gmail.com>
2020-07-16 17:16:19 +0300

    adding more tests to cover snapshot functionaity for derivatives status

--- pkg/models/derivatives/derivatives_test.go
+++ pkg/models/derivatives/derivatives_test.go
@@ -12,7 +12,7 @@ func TestNewDerivativeStatusFromWsRaw(t *testing.T) {
 	t.Run("insufficient arguments", func(t *testing.T) {
 		payload := []interface{}{float64(1591614631576)}
 
-		d, err := derivatives.NewDerivativeStatusFromWsRaw("tBTCF0:USTF0", payload)
+		d, err := derivatives.FromWsRaw("tBTCF0:USTF0", payload)
 		require.NotNil(t, err)
 		require.Nil(t, d)
 	})
@@ -39,7 +39,7 @@ func TestNewDerivativeStatusFromWsRaw(t *testing.T) {
 			3813.72957182,
 		}
 
-		d, err := derivatives.NewDerivativeStatusFromWsRaw("tBTCF0:USTF0", payload)
+		d, err := derivatives.FromWsRaw("tBTCF0:USTF0", payload)
 		require.Nil(t, err)
 
 		expected := &derivatives.DerivativeStatus{
@@ -49,6 +49,7 @@ func TestNewDerivativeStatusFromWsRaw(t *testing.T) {
 			SpotPrice:            9275.3,
 			InsuranceFundBalance: 1.39147227686063e+06,
 			FundingAccrued:       -0.00011968,
+			FundingStep:          3144,
 			MarkPrice:            9276.06,
 			OpenInterest:         3813.72957182,
 		}
@@ -60,7 +61,7 @@ func TestNewDerivativeStatusFromRaw(t *testing.T) {
 	t.Run("insufficient arguments", func(t *testing.T) {
 		payload := []interface{}{"tBTCF0:USTF0"}
 
-		d, err := derivatives.NewDerivativeStatusFromRaw(payload)
+		d, err := derivatives.FromRaw(payload)
 		require.NotNil(t, err)
 		require.Nil(t, d)
 	})
@@ -88,7 +89,7 @@ func TestNewDerivativeStatusFromRaw(t *testing.T) {
 			3813.72957182,
 		}
 
-		d, err := derivatives.NewDerivativeStatusFromRaw(payload)
+		d, err := derivatives.FromRaw(payload)
 		require.Nil(t, err)
 
 		expected := &derivatives.DerivativeStatus{
@@ -98,9 +99,97 @@ func TestNewDerivativeStatusFromRaw(t *testing.T) {
 			SpotPrice:            9275.3,
 			InsuranceFundBalance: 1.39147227686063e+06,
 			FundingAccrued:       -0.00011968,
+			FundingStep:          3144,
 			MarkPrice:            9276.06,
 			OpenInterest:         3813.72957182,
 		}
 		assert.Equal(t, expected, d)
+	})
+}
+
+func TestSnapshotFromRaw(t *testing.T) {
+	t.Run("invalid arguments", func(t *testing.T) {
+		payload := [][]interface{}{{"tBTCF0:USTF0"}}
+		ss, err := derivatives.SnapshotFromRaw(payload)
+		require.NotNil(t, err)
+		require.Nil(t, ss)
+	})
+
+	t.Run("valid arguments", func(t *testing.T) {
+		payload := [][]interface{}{
+			{
+				"tBTCF0:USTF0",
+				float64(1591614631576),
+				nil,
+				9271.1234567,
+				9275.3,
+				nil,
+				1391472.27686063,
+				nil,
+				1594656000000,
+				-0.00011968,
+				3144,
+				nil,
+				0,
+				nil,
+				nil,
+				9276.06,
+				nil,
+				nil,
+				3813.72957182,
+			},
+			{
+				"tBTCF0:USTF0",
+				float64(1591614631576),
+				nil,
+				9271.1234567,
+				9275.3,
+				nil,
+				1391472.27686063,
+				nil,
+				1594656000000,
+				-0.00011968,
+				3200,
+				nil,
+				0,
+				nil,
+				nil,
+				9276.06,
+				nil,
+				nil,
+				3813.72957182,
+			},
+		}
+		ss, err := derivatives.SnapshotFromRaw(payload)
+		require.Nil(t, err)
+
+		expected := &derivatives.DerivativeStatusSnapshot{
+			Snapshot: []*derivatives.DerivativeStatus{
+				{
+					Symbol:               "tBTCF0:USTF0",
+					MTS:                  1591614631576,
+					Price:                9271.1234567,
+					SpotPrice:            9275.3,
+					InsuranceFundBalance: 1.39147227686063e+06,
+					FundingAccrued:       -0.00011968,
+					FundingStep:          3144,
+					MarkPrice:            9276.06,
+					OpenInterest:         3813.72957182,
+				},
+				{
+					Symbol:               "tBTCF0:USTF0",
+					MTS:                  1591614631576,
+					Price:                9271.1234567,
+					SpotPrice:            9275.3,
+					InsuranceFundBalance: 1.39147227686063e+06,
+					FundingAccrued:       -0.00011968,
+					FundingStep:          3200,
+					MarkPrice:            9276.06,
+					OpenInterest:         3813.72957182,
+				},
+			},
+		}
+
+		assert.Equal(t, expected, ss)
 	})
 }
