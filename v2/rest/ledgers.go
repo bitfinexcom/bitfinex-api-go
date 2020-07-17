28a56b162922e4efa7d27ab4254051523519f6ee
Daniel1984 <aciddaniel@gmail.com>
2020-07-17 16:09:07 +0300

    v2/rest/ledgers.go implementing new Ledger type, updating params

--- v2/rest/ledgers.go
+++ v2/rest/ledgers.go
@@ -1,9 +1,11 @@
 package rest
 
 import (
-	"github.com/bitfinexcom/bitfinex-api-go/v2"
-	"path"
 	"fmt"
+	"path"
+
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/ledger"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 )
 
 // LedgerService manages the Ledgers endpoint.
@@ -12,26 +14,30 @@ type LedgerService struct {
 	Synchronous
 }
 
-// Retrieves all of the past ledger entreies
+var maxLimit int32 = 2500
+
+// Ledgers - all of the past ledger entreies
 // see https://docs.bitfinex.com/reference#ledgers for more info
-func (s *LedgerService) Ledgers(currency string, start int64, end int64, max int32) (*bitfinex.LedgerSnapshot, error) {
-    if max > 500 {
-    	return nil, fmt.Errorf("Max request limit is higher then 500 : %#v", max)
-    }
+func (s *LedgerService) Ledgers(currency string, start int64, end int64, max int32) (*ledger.Snapshot, error) {
+	if max > maxLimit {
+		return nil, fmt.Errorf("Max request limit:%d, got: %d", maxLimit, max)
+	}
 
-	req, err := s.requestFactory.NewAuthenticatedRequestWithData(bitfinex.PermissionRead, path.Join("ledgers", currency, "hist"), map[string]interface{}{"start": start, "end": end, "limit": max})
+	payload := map[string]interface{}{"start": start, "end": end, "limit": max}
+	req, err := s.requestFactory.NewAuthenticatedRequestWithData(bitfinex.PermissionRead, path.Join("ledgers", currency, "hist"), payload)
 	if err != nil {
 		return nil, err
 	}
+
 	raw, err := s.Request(req)
-
 	if err != nil {
 		return nil, err
 	}
 
-	os, err := bitfinex.NewLedgerSnapshotFromRaw(raw)
+	lss, err := ledger.SnapshotFromRaw(raw, ledger.FromRaw)
 	if err != nil {
 		return nil, err
 	}
-	return os, nil
+
+	return lss, nil
 }
