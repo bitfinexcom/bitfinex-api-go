009359ec1d85039c541913b65be0223f097b387f
Daniel1984 <aciddaniel@gmail.com>
2020-07-20 13:53:52 +0300

    rest tickers example usage

--- examples/v2/rest-ticker/main.go
+++ examples/v2/rest-ticker/main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"log"
-	bfx "github.com/bitfinexcom/bitfinex-api-go/v2"
-	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
-)
 
+	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
+	"github.com/davecgh/go-spew/spew"
+)
 
 func main() {
 	c := rest.NewClient()
-	symbols := []string{bfx.TradingPrefix+bfx.BTCUSD, bfx.TradingPrefix+bfx.EOSBTC}
-	tickers, err := c.Tickers.GetMulti(symbols)
+
+	get(c)
+	getMulti(c)
+	all(c)
+}
+
+func get(c *rest.Client) {
+	t, err := c.Tickers.Get("tBTCUSD")
 
 	if err != nil {
-		log.Fatalf("getting ticker: %s", err)
+		log.Fatalf("get: %s", err)
 	}
 
-	log.Print(tickers)
+	spew.Dump(t)
+}
+
+func getMulti(c *rest.Client) {
+	symbols := []string{"tBTCUSD", "tEOSBTC"}
+	tm, err := c.Tickers.GetMulti(symbols)
+
+	if err != nil {
+		log.Fatalf("getMulti: %s", err)
+	}
+
+	spew.Dump(tm)
+}
+
+func all(c *rest.Client) {
+	t, err := c.Tickers.All()
+
+	if err != nil {
+		log.Fatalf("all: %s", err)
+	}
+
+	spew.Dump(t)
 }
