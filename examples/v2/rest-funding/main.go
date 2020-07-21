f510c79b1b2455e6a5286a0c90c2168e3808d979
Daniel1984 <aciddaniel@gmail.com>
2020-07-21 10:12:57 +0300

    enhancing rest-funding example usage to follow pattern

--- examples/v2/rest-funding/main.go
+++ examples/v2/rest-funding/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-// Set BFX_APIKEY and BFX_SECRET as :
+// Set BFX_API_KEY and BFX_API_SECRET as :
 //
 // export BFX_API_KEY=YOUR_API_KEY
 // export BFX_API_SECRET=YOUR_API_SECRET
@@ -18,76 +18,18 @@ import (
 // you can obtain it from https://www.bitfinex.com/api
 
 func main() {
-	key := os.Getenv("BFX_KEY")
-	secret := os.Getenv("BFX_SECRET")
-	c := rest.NewClientWithURL("https://test.bitfinex.com/v2/").Credentials(key, secret)
-	// active funding offers
-	snap, err := c.Funding.Offers("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range snap.Snapshot {
-		fmt.Println(item)
-	}
-	// funding offer history
-	snapHist, err := c.Funding.OfferHistory("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range snapHist.Snapshot {
-		fmt.Println(item)
-	}
-	// active loans
-	snapLoans, err := c.Funding.Loans("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range snapLoans.Snapshot {
-		fmt.Println(item)
-	}
-	// loans history
-	napLoansHist, err := c.Funding.LoansHistory("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range napLoansHist.Snapshot {
-		fmt.Println(item)
-	}
-	// active credits
-	snapCredits, err := c.Funding.Credits("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range snapCredits.Snapshot {
-		fmt.Println(item)
-	}
-	// credits history
-	napCreditsHist, err := c.Funding.CreditsHistory("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range napCreditsHist.Snapshot {
-		fmt.Println(item)
-	}
-	// funding trades
-	napTradesHist, err := c.Funding.Trades("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range napTradesHist.Snapshot {
-		fmt.Println(item)
-	}
+	key := os.Getenv("BFX_API_KEY")
+	secret := os.Getenv("BFX_API_SECRET")
+	c := rest.NewClient().Credentials(key, secret)
 
-	// keep funding
-	resp, err := c.Funding.KeepFunding(rest.KeepFundingRequest{
-		Type: "credit",
-		ID:   12345, // Insert correct ID
-	})
-	if err != nil {
-		log.Fatalf("KeepFunding error: %s", err)
-	}
-
-	spew.Dump(resp)
+	offers(c)
+	offerHistory(c)
+	loans(c)
+	loansHistory(c)
+	activeCredits(c)
+	creditsHistory(c)
+	fundingTrades(c)
+	keepFunding(c)
 
 	/********* submit a new funding offer ***********/
 	fo, err := c.Funding.SubmitOffer(&bitfinex.FundingOfferRequest{
@@ -102,6 +44,7 @@ func main() {
 		panic(err)
 	}
 	newOffer := fo.NotifyInfo.(*bitfinex.FundingOfferNew)
+
 	/********* cancel funding offer ***********/
 	fc, err := c.Funding.CancelOffer(&bitfinex.FundingOfferCancelRequest{
 		Id: newOffer.ID,
@@ -110,4 +53,91 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(fc)
+}
+
+func offers(c *rest.Client) {
+	// active funding offers
+	snap, err := c.Funding.Offers("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range snap.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func offerHistory(c *rest.Client) {
+	// funding offer history
+	snapHist, err := c.Funding.OfferHistory("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range snapHist.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func loans(c *rest.Client) {
+	// active loans
+	snapLoans, err := c.Funding.Loans("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range snapLoans.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func loansHistory(c *rest.Client) {
+	napLoansHist, err := c.Funding.LoansHistory("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range napLoansHist.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func activeCredits(c *rest.Client) {
+	// active credits
+	snapCredits, err := c.Funding.Credits("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range snapCredits.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func creditsHistory(c *rest.Client) {
+	napCreditsHist, err := c.Funding.CreditsHistory("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range napCreditsHist.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func fundingTrades(c *rest.Client) {
+	napTradesHist, err := c.Funding.Trades("fUSD")
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range napTradesHist.Snapshot {
+		fmt.Println(item)
+	}
+}
+
+func keepFunding(c *rest.Client) {
+	// keep funding
+	resp, err := c.Funding.KeepFunding(rest.KeepFundingRequest{
+		Type: "credit",
+		ID:   12345, // Insert correct ID
+	})
+	if err != nil {
+		log.Fatalf("KeepFunding error: %s", err)
+	}
+
+	spew.Dump(resp)
 }
