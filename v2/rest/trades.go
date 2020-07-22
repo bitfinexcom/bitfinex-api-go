e1b19a1108c46c59c2aff06ea0e11a6b69e00295
Daniel1984 <aciddaniel@gmail.com>
2020-07-22 12:22:17 +0300

    v2/rest/trades.go putting new tradeexecutionupdate package to work

--- v2/rest/trades.go
+++ v2/rest/trades.go
@@ -1,10 +1,14 @@
 package rest
 
 import (
-	"github.com/bitfinexcom/bitfinex-api-go/v2"
 	"net/url"
 	"path"
 	"strconv"
+
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/convert"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/trade"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/tradeexecutionupdate"
+	"github.com/bitfinexcom/bitfinex-api-go/v2"
 )
 
 // TradeService manages the Trade endpoint.
@@ -15,7 +19,7 @@ type TradeService struct {
 
 // All returns all orders for the authenticated account.
 // left this in her
-func (s *TradeService) allAccountWithSymbol(symbol string) (*bitfinex.TradeExecutionUpdateSnapshot, error) {
+func (s *TradeService) allAccountWithSymbol(symbol string) (*tradeexecutionupdate.Snapshot, error) {
 	req, err := s.requestFactory.NewAuthenticatedRequest(bitfinex.PermissionRead, path.Join("trades", symbol, "hist"))
 	if err != nil {
 		return nil, err
@@ -27,7 +31,7 @@ func (s *TradeService) allAccountWithSymbol(symbol string) (*bitfinex.TradeExecu
 	return parseRawPrivateToSnapshot(raw)
 }
 
-func (s *TradeService) allAccount() (*bitfinex.TradeExecutionUpdateSnapshot, error) {
+func (s *TradeService) allAccount() (*tradeexecutionupdate.Snapshot, error) {
 	req, err := s.requestFactory.NewAuthenticatedRequest(bitfinex.PermissionRead, path.Join("trades", "hist"))
 	if err != nil {
 		return nil, err
@@ -41,13 +45,13 @@ func (s *TradeService) allAccount() (*bitfinex.TradeExecutionUpdateSnapshot, err
 
 // Retrieves all matched trades for the account
 // see https://docs.bitfinex.com/reference#rest-auth-trades-hist for more info
-func (s *TradeService) AccountAll() (*bitfinex.TradeExecutionUpdateSnapshot, error) {
+func (s *TradeService) AccountAll() (*tradeexecutionupdate.Snapshot, error) {
 	return s.allAccount()
 }
 
 // Retrieves all matched trades with the given symbol for the account
 // see https://docs.bitfinex.com/reference#rest-auth-trades-hist for more info
-func (s *TradeService) AccountAllWithSymbol(symbol string) (*bitfinex.TradeExecutionUpdateSnapshot, error) {
+func (s *TradeService) AccountAllWithSymbol(symbol string) (*tradeexecutionupdate.Snapshot, error) {
 	return s.allAccountWithSymbol(symbol)
 }
 
@@ -59,7 +63,7 @@ func (s *TradeService) AccountHistoryWithQuery(
 	end bitfinex.Mts,
 	limit bitfinex.QueryLimit,
 	sort bitfinex.SortOrder,
-	) (*bitfinex.TradeExecutionUpdateSnapshot, error) {
+) (*tradeexecutionupdate.Snapshot, error) {
 	req, err := s.requestFactory.NewAuthenticatedRequest(bitfinex.PermissionRead, path.Join("trades", symbol, "hist"))
 	if err != nil {
 		return nil, err
@@ -84,42 +88,30 @@ func (s *TradeService) PublicHistoryWithQuery(
 	end bitfinex.Mts,
 	limit bitfinex.QueryLimit,
 	sort bitfinex.SortOrder,
-	) (*bitfinex.TradeSnapshot, error) {
-		req := NewRequestWithMethod(path.Join("trades", symbol, "hist"), "GET")
-		req.Params = make(url.Values)
-		req.Params.Add("end", strconv.FormatInt(int64(end), 10))
-		req.Params.Add("start", strconv.FormatInt(int64(start), 10))
-		req.Params.Add("limit", strconv.FormatInt(int64(limit), 10))
-		req.Params.Add("sort", strconv.FormatInt(int64(sort), 10))
-		raw, err := s.Request(req)
-		if err != nil {
-			return nil, err
-		}
-		return parseRawPublicToSnapshot(symbol, raw)
+) (*trade.Snapshot, error) {
+	req := NewRequestWithMethod(path.Join("trades", symbol, "hist"), "GET")
+	req.Params = make(url.Values)
+	req.Params.Add("end", strconv.FormatInt(int64(end), 10))
+	req.Params.Add("start", strconv.FormatInt(int64(start), 10))
+	req.Params.Add("limit", strconv.FormatInt(int64(limit), 10))
+	req.Params.Add("sort", strconv.FormatInt(int64(sort), 10))
+	raw, err := s.Request(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(raw) <= 0 {
+		return &trade.Snapshot{Snapshot: make([]*trade.Trade, 0)}, nil
+	}
+
+	return trade.SnapshotFromRaw(symbol, convert.ToInterfaceArray(raw))
 }
 
-func parseRawPublicToSnapshot(symbol string, raw []interface{}) (*bitfinex.TradeSnapshot, error) {
+func parseRawPrivateToSnapshot(raw []interface{}) (*tradeexecutionupdate.Snapshot, error) {
 	if len(raw) <= 0 {
-		// return empty
-		return &bitfinex.TradeSnapshot{Snapshot: make([]*bitfinex.Trade, 0)}, nil
+		return &tradeexecutionupdate.Snapshot{Snapshot: make([]*tradeexecutionupdate.TradeExecutionUpdate, 0)}, nil
 	}
-	// convert to array of floats
-	dat := make([][]float64, 0)
-	for _, r := range raw {
-		t := []float64{}
-		for _, r2 := range r.([]interface{}) {
-			t = append(t, r2.(float64))
-		}
-		dat = append(dat, t)
-	}
-	return bitfinex.NewTradeSnapshotFromRaw(symbol, dat)
-}
-
-func parseRawPrivateToSnapshot(raw []interface{}) (*bitfinex.TradeExecutionUpdateSnapshot, error) {
-	if len(raw) <= 0 {
-		return &bitfinex.TradeExecutionUpdateSnapshot{Snapshot: make([]*bitfinex.TradeExecutionUpdate, 0)}, nil
-	}
-	tradeExecutions, err := bitfinex.NewTradeExecutionUpdateSnapshotFromRaw(raw)
+	tradeExecutions, err := tradeexecutionupdate.SnapshotFromRaw(raw)
 	if err != nil {
 		return nil, err
 	}
