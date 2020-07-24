a6bee425220812f7817b58035ae1ee40b880e58b
Daniel1984 <aciddaniel@gmail.com>
2020-07-24 13:51:14 +0300

    v2/types.go cleanup

--- v2/types.go
+++ v2/types.go
@@ -122,86 +122,8 @@ const (
 	PriceLevelDefault int = 25
 )
 
-type bookFrequency string
-
 // BookFrequency provides a typed book frequency.
-type BookFrequency bookFrequency
-
-// marginInfoFromRaw returns either a MarginInfoBase or MarginInfoUpdate, since
-// the Margin Info is split up into a base and per symbol parts.
-func NewMarginInfoFromRaw(raw []interface{}) (o interface{}, err error) {
-	if len(raw) < 2 {
-		return o, fmt.Errorf("data slice too short for margin info base: %#v", raw)
-	}
-
-	typ, ok := raw[0].(string)
-	if !ok {
-		return o, fmt.Errorf("expected margin info type in first position for margin info but got %#v", raw)
-	}
-
-	if len(raw) == 2 && typ == "base" { // This should be ["base", [...]]
-		data, ok := raw[1].([]interface{})
-		if !ok {
-			return o, fmt.Errorf("expected margin info array in second position for margin info but got %#v", raw)
-		}
-
-		return NewMarginInfoBaseFromRaw(data)
-	} else if len(raw) == 3 && typ == "sym" { // This should be ["sym", SYMBOL, [...]]
-		symbol, ok := raw[1].(string)
-		if !ok {
-			return o, fmt.Errorf("expected margin info symbol in second position for margin info update but got %#v", raw)
-		}
-
-		data, ok := raw[2].([]interface{})
-		if !ok {
-			return o, fmt.Errorf("expected margin info array in third position for margin info update but got %#v", raw)
-		}
-
-		return NewMarginInfoUpdateFromRaw(symbol, data)
-	}
-
-	return nil, fmt.Errorf("invalid margin info type in %#v", raw)
-}
-
-type MarginInfoUpdate struct {
-	Symbol          string
-	TradableBalance float64
-}
-
-func NewMarginInfoUpdateFromRaw(symbol string, raw []interface{}) (o *MarginInfoUpdate, err error) {
-	if len(raw) < 1 {
-		return o, fmt.Errorf("data slice too short for margin info update: %#v", raw)
-	}
-
-	o = &MarginInfoUpdate{
-		Symbol:          symbol,
-		TradableBalance: convert.F64ValOrZero(raw[0]),
-	}
-
-	return
-}
-
-type MarginInfoBase struct {
-	UserProfitLoss float64
-	UserSwaps      float64
-	MarginBalance  float64
-	MarginNet      float64
-}
-
-func NewMarginInfoBaseFromRaw(raw []interface{}) (o *MarginInfoBase, err error) {
-	if len(raw) < 4 {
-		return o, fmt.Errorf("data slice too short for margin info base: %#v", raw)
-	}
-
-	o = &MarginInfoBase{
-		UserProfitLoss: convert.F64ValOrZero(raw[0]),
-		UserSwaps:      convert.F64ValOrZero(raw[1]),
-		MarginBalance:  convert.F64ValOrZero(raw[2]),
-		MarginNet:      convert.F64ValOrZero(raw[3]),
-	}
-
-	return
-}
+type BookFrequency string
 
 type Notification struct {
 	MTS        int64
