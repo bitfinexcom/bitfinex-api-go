768ef8e028cacf5ebca82aacd23e4f5e1108feb8
Daniel1984 <aciddaniel@gmail.com>
2020-12-21 14:41:20 +0200

    test coverage for candle model FromWSRaw functionality

--- pkg/models/candle/candle_test.go
+++ pkg/models/candle/candle_test.go
@@ -107,3 +107,105 @@ func TestSnapshotFromRaw(t *testing.T) {
 		assert.Equal(t, expected, ss)
 	})
 }
+
+func TestFromWSRaw(t *testing.T) {
+	t.Run("invalid arguments", func(t *testing.T) {
+		payload := []interface{}{1.5948918e+12}
+		c, err := candle.FromWSRaw("trade:1m:tBTCUSD", payload)
+		require.NotNil(t, err)
+		require.Nil(t, c)
+	})
+
+	t.Run("missing arguments", func(t *testing.T) {
+		payload := []interface{}{}
+		c, err := candle.FromWSRaw("trade:1m:tBTCUSD", payload)
+		require.NotNil(t, err)
+		require.Nil(t, c)
+	})
+
+	t.Run("invalid key", func(t *testing.T) {
+		payload := []interface{}{}
+		c, err := candle.FromWSRaw("tBTCUSD", payload)
+		require.NotNil(t, err)
+		require.Nil(t, c)
+	})
+
+	t.Run("valid candle arguments", func(t *testing.T) {
+		payload := []interface{}{
+			1.5948918e+12,
+			9100,
+			9076.9,
+			9100.1,
+			9054.1,
+			149.87216331,
+		}
+
+		w, err := candle.FromWSRaw("trade:1m:tBTCUSD", payload)
+		require.Nil(t, err)
+
+		expected := &candle.Candle{
+			Symbol:     "tBTCUSD",
+			Resolution: "1m",
+			MTS:        1594891800000,
+			Open:       9100,
+			Close:      9076.9,
+			High:       9100.1,
+			Low:        9054.1,
+			Volume:     149.87216331,
+		}
+
+		assert.Equal(t, expected, w)
+	})
+
+	t.Run("valid snapshot arguments", func(t *testing.T) {
+		payload := []interface{}{
+			[]interface{}{
+				1.5948918e+12,
+				9100,
+				9076.9,
+				9100.1,
+				9054.1,
+				149.87216331,
+			},
+			[]interface{}{
+				1.5948918e+12,
+				9200,
+				9076.9,
+				9100.1,
+				9054.1,
+				149.87216331,
+			},
+		}
+
+		ss, err := candle.FromWSRaw("trade:1m:tBTCUSD", payload)
+		require.Nil(t, err)
+
+		expected := &candle.Snapshot{
+			Snapshot: []*candle.Candle{
+				{
+					Symbol:     "tBTCUSD",
+					Resolution: "1m",
+					MTS:        1594891800000,
+					Open:       9100,
+					Close:      9076.9,
+					High:       9100.1,
+					Low:        9054.1,
+					Volume:     149.87216331,
+				},
+				{
+					Symbol:     "tBTCUSD",
+					Resolution: "1m",
+					MTS:        1594891800000,
+					Open:       9200,
+					Close:      9076.9,
+					High:       9100.1,
+					Low:        9054.1,
+					Volume:     149.87216331,
+				},
+			},
+		}
+
+		require.Nil(t, err)
+		assert.Equal(t, expected, ss)
+	})
+}
