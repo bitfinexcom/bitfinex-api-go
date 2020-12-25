70fafd0b4e4ef0437df0de813a56db479d84d0f1
Daniel1984 <aciddaniel@gmail.com>
2020-12-25 21:30:16 +0200

    amending mux client with ability to create authenticated client

--- pkg/mux/client/client.go
+++ pkg/mux/client/client.go
@@ -2,33 +2,35 @@ package client
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
 	"encoding/json"
 	"net"
 
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/msg"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/subs"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/utils"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
-type Msg struct {
-	Data []byte
-	Err  error
-	CID  int
-}
-
 type Client struct {
-	Conn net.Conn
-	Subs *subs.Subs
-	Err  error
-	ID   int
+	Conn     net.Conn
+	Subs     *subs.Subs
+	Err      error
+	ID       int
+	isPublic bool
+	nonceGen *utils.EpochNonceGenerator
 }
 
 // New returns pointer to Client instance
 func New(ID int) *Client {
 	return &Client{
-		ID:   ID,
-		Subs: subs.New(),
+		ID:       ID,
+		Subs:     subs.New(),
+		nonceGen: utils.NewEpochNonceGenerator(),
 	}
 }
 
@@ -38,8 +40,39 @@ func (c *Client) Public() *Client {
 		return c
 	}
 
-	c.Subs.SubsLimit = 30
+	c.isPublic = true
+	c.Subs.SubsLimit = 25
 	c.Conn, _, _, c.Err = ws.DefaultDialer.Dial(context.Background(), "wss://api-pub.bitfinex.com/ws/2")
+	return c
+}
+
+// Private creates and returns private client to interact with private channels
+func (c *Client) Private(key, sec string) *Client {
+	if c.Err != nil {
+		return c
+	}
+
+	// nonce := fmt.Sprintf("%v", time.Now().UTC().UnixNano()/1000)
+	nonce := c.nonceGen.GetNonce()
+	c.Subs.SubsLimit = 20
+	c.Conn, _, _, c.Err = ws.DefaultDialer.Dial(context.Background(), "wss://api.staging.bitfinex.com/ws/2")
+	if c.Err != nil {
+		return c
+	}
+
+	payload := "AUTH" + nonce
+	sig := hmac.New(sha512.New384, []byte(sec))
+	sig.Write([]byte(payload))
+	pldSign := hex.EncodeToString(sig.Sum(nil))
+	sub := event.Subscribe{
+		Event:       "auth",
+		APIKEY:      key,
+		AuthSig:     pldSign,
+		AuthPayload: payload,
+		AuthNonce:   nonce,
+	}
+
+	c.Subscribe(sub)
 	return c
 }
 
@@ -59,15 +92,25 @@ func (c *Client) Subscribe(sub event.Subscribe) *Client {
 	return c
 }
 
-func (c *Client) Read(ch chan<- Msg) {
+func (c *Client) Read(ch chan<- msg.Msg) {
 	for {
-		msg, _, err := wsutil.ReadServerData(c.Conn)
+		ms, _, err := wsutil.ReadServerData(c.Conn)
 		if err != nil {
 			c.Conn.Close()
-			ch <- Msg{nil, err, c.ID}
+			ch <- msg.Msg{
+				Data:     nil,
+				Err:      err,
+				CID:      c.ID,
+				IsPublic: c.isPublic,
+			}
 			return
 		}
 
-		ch <- Msg{msg, nil, c.ID}
+		ch <- msg.Msg{
+			Data:     ms,
+			Err:      nil,
+			CID:      c.ID,
+			IsPublic: c.isPublic,
+		}
 	}
 }
