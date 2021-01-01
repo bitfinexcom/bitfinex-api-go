992a9d7a49c404e449e4d46ece69787452e5c502
Daniel1984 <aciddaniel@gmail.com>
2021-01-02 01:28:24 +0200

    example of submitting new order

---
+++ examples/v2/ws-mux-submit-order/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/order"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux"
+)
+
+func main() {
+	m := mux.New().
+		TransformRaw().
+		WithAPIKEY("YOUR_API_KEY").
+		WithAPISEC("YOUR_API_SECRET").
+		Start()
+
+	crash := make(chan error)
+	auth := make(chan bool)
+
+	go func() {
+		crash <- m.Listen(func(msg interface{}, err error) {
+			if err != nil {
+				log.Printf("error received: %s\n", err)
+			}
+
+			switch v := msg.(type) {
+			case event.Info:
+				if v.Event == "auth" && v.Status == "OK" {
+					auth <- true
+				}
+			case order.New:
+				log.Printf("%T: %+v\n", v, v)
+			default:
+				log.Printf("unrecognized: %T: %+v\n", v, v)
+			}
+		})
+	}()
+
+	for {
+		select {
+		case err := <-crash:
+			log.Fatal(err)
+		case <-auth:
+			// authenticated, safe to submit orders etc
+			if err := m.Send(&order.NewRequest{
+				CID:    788,
+				Type:   "EXCHANGE LIMIT",
+				Symbol: "tBTCUSD",
+				Price:  33,
+				Amount: 0.001,
+			}); err != nil {
+				fmt.Printf("err submitting new order: %s\n", err)
+			}
+		}
+	}
+}
