405366d9482f55c2766c1b5c864001bb99d4c844
Daniel1984 <aciddaniel@gmail.com>
2021-01-06 11:09:07 +0200

    ticker, candle and candle snapshot "decode" test coverage

--- pkg/mux/msg/msg_test.go
+++ pkg/mux/msg/msg_test.go
@@ -3,7 +3,9 @@ package msg_test
 import (
 	"testing"
 
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/candle"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/ticker"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/trade"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/msg"
 	"github.com/stretchr/testify/assert"
@@ -41,11 +43,11 @@ func TestIsRaw(t *testing.T) {
 		pld      []byte
 		expected bool
 	}{
-		"raw type": {
+		"raw msg type": {
 			pld:      []byte(`[]`),
 			expected: true,
 		},
-		"not raw type": {
+		"raw info type": {
 			pld:      []byte(`{}`),
 			expected: false,
 		},
@@ -65,7 +67,14 @@ func TestIsRaw(t *testing.T) {
 
 func TestProcessEvent(t *testing.T) {
 	m := msg.Msg{
-		Data: []byte(`{"event":"info","version":2,"serverId":"dbea77ee-4740-4a82-84f3-c6bc1b5abb9a","platform":{"status":1}}`),
+		Data: []byte(`{
+			"event": "info",
+			"version": 2,
+			"serverId": "dbea77ee-4740-4a82-84f3-c6bc1b5abb9a",
+			"platform": {
+				"status":1
+			}
+		}`),
 	}
 
 	expected := event.Info{
@@ -88,7 +97,7 @@ func TestProcessRaw(t *testing.T) {
 		expected interface{}
 		inf      map[int64]event.Info
 	}{
-		"raw info event": {
+		"info event": {
 			pld: []byte(`[123, "hb"]`),
 			inf: map[int64]event.Info{123: {}},
 			expected: event.Info{
@@ -96,7 +105,7 @@ func TestProcessRaw(t *testing.T) {
 				Subscribe: event.Subscribe{Event: "hb"},
 			},
 		},
-		"raw trades snapshot": {
+		"trades snapshot": {
 			pld: []byte(`[111,[[559273857,1609665708633,-0.0048,34113]]]`),
 			inf: map[int64]event.Info{
 				111: {
@@ -118,7 +127,7 @@ func TestProcessRaw(t *testing.T) {
 				},
 			},
 		},
-		"raw trade": {
+		"trade": {
 			pld: []byte(`[111,[559273857,1609665708633,-0.0048,34113]]`),
 			inf: map[int64]event.Info{
 				111: {
@@ -134,6 +143,82 @@ func TestProcessRaw(t *testing.T) {
 				MTS:    1609665708633,
 				Amount: -0.0048,
 				Price:  34113,
+			},
+		},
+		"ticker": {
+			pld: []byte(`[
+				111,
+				[
+					34072,0.019999999999999997,34080,6.69793272,4350,
+					0.1464,34062,4047.85335915,34758,29490
+				]
+			]`),
+			inf: map[int64]event.Info{
+				111: {
+					Subscribe: event.Subscribe{
+						Channel: "ticker",
+						Symbol:  "tBTCUST",
+					},
+				},
+			},
+			expected: &ticker.Ticker{
+				Symbol:          "tBTCUST",
+				Bid:             34072,
+				BidSize:         0.019999999999999997,
+				Ask:             34080,
+				AskSize:         6.69793272,
+				DailyChange:     4350,
+				DailyChangePerc: 0.1464,
+				LastPrice:       34062,
+				Volume:          4047.85335915,
+				High:            34758,
+				Low:             29490,
+			},
+		},
+		"candles snapshot": {
+			pld: []byte(`[111,[[1609668540000,828.01,827.67,828.42,827.67,2.32080241]]]`),
+			inf: map[int64]event.Info{
+				111: {
+					Subscribe: event.Subscribe{
+						Channel: "candles",
+						Key:     "trade:1m:tETHUST",
+					},
+				},
+			},
+			expected: &candle.Snapshot{
+				Snapshot: []*candle.Candle{
+					{
+						Symbol:     "tETHUST",
+						Resolution: "1m",
+						MTS:        1609668540000,
+						Open:       828.01,
+						Close:      827.67,
+						High:       828.42,
+						Low:        827.67,
+						Volume:     2.32080241,
+					},
+				},
+			},
+		},
+		"candle": {
+			pld: []byte(`[111,[1609668540000,828.01,827.67,828.42,827.67,2.32080241]]`),
+			inf: map[int64]event.Info{
+				111: {
+					Subscribe: event.Subscribe{
+						Channel: "candles",
+						Key:     "trade:1m:tETHUST",
+					},
+				},
+			},
+			expected: &candle.Candle{
+				Symbol:     "tETHUST",
+				Resolution: "1m",
+				MTS:        1609668540000,
+				Open:       828.01,
+				Close:      827.67,
+				High:       828.42,
+				Low:        827.67,
+				Volume:     2.32080241,
 			},
 		},
 	}
