0f6b9d4342102e3cccccb071580f6ab3f415bb3e
Daniel1984 <aciddaniel@gmail.com>
2021-01-06 09:19:14 +0200

    testing msg ability to process event type msg

--- pkg/mux/msg/msg_test.go
+++ pkg/mux/msg/msg_test.go
@@ -3,6 +3,7 @@ package msg_test
 import (
 	"testing"
 
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/msg"
 	"github.com/stretchr/testify/assert"
 )
@@ -59,4 +60,23 @@ func TestIsRaw(t *testing.T) {
 			assert.Equal(t, v.expected, got)
 		})
 	}
+}
+
+func TestProcessEvent(t *testing.T) {
+	m := msg.Msg{
+		Data: []byte(`{"event":"info","version":2,"serverId":"dbea77ee-4740-4a82-84f3-c6bc1b5abb9a","platform":{"status":1}}`),
+	}
+
+	expected := event.Info{
+		Subscribe: event.Subscribe{
+			Event: "info",
+		},
+		Version:  2,
+		ServerID: "dbea77ee-4740-4a82-84f3-c6bc1b5abb9a",
+		Platform: event.Platform{Status: 1},
+	}
+
+	got, err := m.ProcessEvent()
+	assert.NoError(t, err)
+	assert.Equal(t, expected, got)
 }
