80174116f5ab2305e60ca435c0bb6a92e7752e78
Daniel1984 <aciddaniel@gmail.com>
2021-01-06 10:01:13 +0200

    msg raw info event, trade and trade snapshot "decode" test coverage

--- pkg/mux/msg/msg_test.go
+++ pkg/mux/msg/msg_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
+	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/trade"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/msg"
 	"github.com/stretchr/testify/assert"
 )
@@ -79,4 +80,70 @@ func TestProcessEvent(t *testing.T) {
 	got, err := m.ProcessEvent()
 	assert.NoError(t, err)
 	assert.Equal(t, expected, got)
+}
+
+func TestProcessRaw(t *testing.T) {
+	cases := map[string]struct {
+		pld      []byte
+		expected interface{}
+		inf      map[int64]event.Info
+	}{
+		"raw info event": {
+			pld: []byte(`[123, "hb"]`),
+			inf: map[int64]event.Info{123: {}},
+			expected: event.Info{
+				ChanID:    123,
+				Subscribe: event.Subscribe{Event: "hb"},
+			},
+		},
+		"raw trades snapshot": {
+			pld: []byte(`[111,[[559273857,1609665708633,-0.0048,34113]]]`),
+			inf: map[int64]event.Info{
+				111: {
+					Subscribe: event.Subscribe{
+						Channel: "trades",
+						Symbol:  "tBTCUST",
+					},
+				},
+			},
+			expected: &trade.Snapshot{
+				Snapshot: []*trade.Trade{
+					{
+						Pair:   "tBTCUST",
+						ID:     559273857,
+						MTS:    1609665708633,
+						Amount: -0.0048,
+						Price:  34113,
+					},
+				},
+			},
+		},
+		"raw trade": {
+			pld: []byte(`[111,[559273857,1609665708633,-0.0048,34113]]`),
+			inf: map[int64]event.Info{
+				111: {
+					Subscribe: event.Subscribe{
+						Channel: "trades",
+						Symbol:  "tBTCUST",
+					},
+				},
+			},
+			expected: &trade.Trade{
+				Pair:   "tBTCUST",
+				ID:     559273857,
+				MTS:    1609665708633,
+				Amount: -0.0048,
+				Price:  34113,
+			},
+		},
+	}
+
+	for k, v := range cases {
+		t.Run(k, func(t *testing.T) {
+			m := msg.Msg{Data: v.pld}
+			got, err := m.ProcessRaw(v.inf)
+			assert.NoError(t, err)
+			assert.Equal(t, v.expected, got)
+		})
+	}
 }
