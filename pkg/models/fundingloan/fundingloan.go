f0e713669ce3f3245c798290d88ee4b108a45ee9
Daniel1984 <aciddaniel@gmail.com>
2021-01-15 14:14:52 +0200

    updating funding loan model and raw data mapping

--- pkg/models/fundingloan/fundingloan.go
+++ pkg/models/fundingloan/fundingloan.go
@@ -1,29 +1,21 @@
 package fundingloan
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/convert"
 )
 
-type New Loan
-type Update Loan
-type Cancel Loan
-
-type Snapshot struct {
-	Snapshot []*Loan
-}
-
 type Loan struct {
 	ID            int64
 	Symbol        string
-	Side          string
+	Side          int
 	MTSCreated    int64
 	MTSUpdated    int64
 	Amount        float64
-	Flags         interface{}
+	Flags         map[string]interface{}
 	Status        string
+	RateType      string
 	Rate          float64
 	Period        int64
 	MTSOpened     int64
@@ -36,27 +28,12 @@ type Loan struct {
 	NoClose       bool
 }
 
-type CancelRequest struct {
-	ID int64
-}
+type New Loan
+type Update Loan
+type Cancel Loan
 
-func (cr *CancelRequest) ToJSON() ([]byte, error) {
-	resp := struct {
-		ID int64 `json:"id"`
-	}{
-		ID: cr.ID,
-	}
-	return json.Marshal(resp)
-}
-
-// MarshalJSON converts the funding loan cancel request into the format required by the
-// bitfinex websocket service.
-func (cr *CancelRequest) MarshalJSON() ([]byte, error) {
-	b, err := cr.ToJSON()
-	if err != nil {
-		return nil, err
-	}
-	return []byte(fmt.Sprintf("[0, \"flc\", null, %s]", string(b))), nil
+type Snapshot struct {
+	Snapshot []*Loan
 }
 
 func FromRaw(raw []interface{}) (l *Loan, err error) {
@@ -67,12 +44,12 @@ func FromRaw(raw []interface{}) (l *Loan, err error) {
 	l = &Loan{
 		ID:            convert.I64ValOrZero(raw[0]),
 		Symbol:        convert.SValOrEmpty(raw[1]),
-		Side:          convert.SValOrEmpty(raw[2]),
+		Side:          convert.ToInt(raw[2]),
 		MTSCreated:    convert.I64ValOrZero(raw[3]),
 		MTSUpdated:    convert.I64ValOrZero(raw[4]),
 		Amount:        convert.F64ValOrZero(raw[5]),
-		Flags:         raw[6],
 		Status:        convert.SValOrEmpty(raw[7]),
+		RateType:      convert.SValOrEmpty(raw[8]),
 		Rate:          convert.F64ValOrZero(raw[11]),
 		Period:        convert.I64ValOrZero(raw[12]),
 		MTSOpened:     convert.I64ValOrZero(raw[13]),
@@ -83,6 +60,10 @@ func FromRaw(raw []interface{}) (l *Loan, err error) {
 		Renew:         convert.BValOrFalse(raw[18]),
 		RateReal:      convert.F64ValOrZero(raw[19]),
 		NoClose:       convert.BValOrFalse(raw[20]),
+	}
+
+	if flags, ok := raw[6].(map[string]interface{}); ok {
+		l.Flags = flags
 	}
 
 	return
