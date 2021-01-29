69243dfd1172159569e676975c018dcc60c54ce3
Daniel1984 <aciddaniel@gmail.com>
2021-01-29 16:48:04 +0200

    on-req snapshot notification raw data mapping to data structure

--- pkg/models/notification/notification_test.go
+++ pkg/models/notification/notification_test.go
@@ -65,6 +65,60 @@ func TestNotificationMapping(t *testing.T) {
 				assert.NoError(t, err)
 			},
 		},
+		"on-req snapshot": {
+			pld: []byte(`[
+				0,
+				"n",
+				[
+					1611922089,"on-req",null,null,
+					[[
+						1201469553,0,788,"tBTCUSD",1611922089073,1611922089073,0.001,0.001,"EXCHANGE LIMIT",
+						null,null,null,0,"ACTIVE",null,null,33,0,0,0,null,null,null,0,0,null,null,null,
+						"API>BFX",null,null,null
+					]],
+					null,"SUCCESS","Submitting exchange limit buy order for 0.001 BTC."
+				]
+			]`),
+			expected: &notification.Notification{
+				MTS:       1611922089,
+				Type:      "on-req",
+				MessageID: 0,
+				NotifyInfo: &order.Snapshot{
+					Snapshot: []*order.Order{
+						{
+							ID:            1201469553,
+							GID:           0,
+							CID:           788,
+							Symbol:        "tBTCUSD",
+							MTSCreated:    1611922089073,
+							MTSUpdated:    1611922089073,
+							Amount:        0.001,
+							AmountOrig:    0.001,
+							Type:          "EXCHANGE LIMIT",
+							TypePrev:      "",
+							MTSTif:        0,
+							Flags:         0,
+							Status:        "ACTIVE",
+							Price:         33,
+							PriceAvg:      0,
+							PriceTrailing: 0,
+							PriceAuxLimit: 0,
+							Notify:        false,
+							Hidden:        false,
+							PlacedID:      0,
+							Routing:       "API>BFX",
+							Meta:          nil,
+						},
+					},
+				},
+				Code:   0,
+				Status: "SUCCESS",
+				Text:   "Submitting exchange limit buy order for 0.001 BTC.",
+			},
+			err: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
 	}
 
 	for k, v := range cases {
