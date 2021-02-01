0b76e2dbcd5d488e29e07a176cb7aad28293b09b
Daniel1984 <aciddaniel@gmail.com>
2021-02-01 14:25:40 +0200

    working towards cleaner ws client interface, better error handling

--- pkg/mux/client/client.go
+++ pkg/mux/client/client.go
@@ -7,64 +7,68 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net"
 
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/event"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/msg"
-	"github.com/bitfinexcom/bitfinex-api-go/pkg/mux/subs"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/utils"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
 type Client struct {
-	Conn     net.Conn
-	Subs     *subs.Subs
-	Err      error
-	ID       int
-	nonceGen *utils.EpochNonceGenerator
+	id        int
+	conn      net.Conn
+	nonceGen  *utils.EpochNonceGenerator
+	subsLimit int
+	subs      map[event.Subscribe]bool
 }
 
 // New returns pointer to Client instance
-func New(ID int) *Client {
+func New() *Client {
 	return &Client{
-		ID:       ID,
-		Subs:     subs.New(),
+		subs:     make(map[event.Subscribe]bool),
 		nonceGen: utils.NewEpochNonceGenerator(),
 	}
 }
 
-// Public creates and returns public client to interact with public channels
-func (c *Client) Public() *Client {
-	if c.Err != nil {
-		return c
-	}
-
-	c.Subs.SubsLimit = 25
-	c.Conn, _, _, c.Err = ws.DefaultDialer.Dial(context.Background(), "wss://api-pub.bitfinex.com/ws/2")
+// WithID assigns clinet ID
+func (c *Client) WithID(ID int) *Client {
+	c.id = ID
 	return c
 }
 
-// Private creates and returns private client to interact with private channels
-func (c *Client) Private(key, sec string) *Client {
-	if c.Err != nil {
-		return c
-	}
+// WithSubsLimit sets limit of subscriptions on the instance
+func (c *Client) WithSubsLimit(limit int) *Client {
+	c.subsLimit = limit
+	return c
+}
 
+// Public creates and returns client to interact with public channels
+func (c *Client) Public(url string) (*Client, error) {
+	conn, _, _, err := ws.DefaultDialer.Dial(context.Background(), url)
+	if err != nil {
+		return nil, err
+	}
+	c.conn = conn
+	return c, nil
+}
+
+// Private creates and returns client to interact with private channels
+func (c *Client) Private(key, sec, url string) (*Client, error) {
 	nonce := c.nonceGen.GetNonce()
-	c.Subs.SubsLimit = 20
-	c.Conn, _, _, c.Err = ws.DefaultDialer.Dial(context.Background(), "wss://api.staging.bitfinex.com/ws/2")
-	if c.Err != nil {
-		return c
+	conn, _, _, err := ws.DefaultDialer.Dial(context.Background(), url)
+	if err != nil {
+		return nil, err
 	}
 
+	c.conn = conn
 	payload := "AUTH" + nonce
 	sig := hmac.New(sha512.New384, []byte(sec))
 	if _, err := sig.Write([]byte(payload)); err != nil {
-		fmt.Printf("err generating sig:%s\n", err)
-		return c
+		return nil, err
 	}
+
 	pldSign := hex.EncodeToString(sig.Sum(nil))
 	sub := event.Subscribe{
 		Event:       "auth",
@@ -74,40 +78,40 @@ func (c *Client) Private(key, sec string) *Client {
 		AuthNonce:   nonce,
 	}
 
-	c.Subscribe(sub)
-	return c
+	if err := c.Subscribe(sub); err != nil {
+		return nil, err
+	}
+	return c, nil
 }
 
-// Subscribe takes subscription payload as per docs and subscribes connection to it
-func (c *Client) Subscribe(sub event.Subscribe) *Client {
-	if c.Err != nil {
-		return c
+// Subscribe takes subscription payload as per docs and subscribes client to it.
+// We keep track of subscriptions so that when client failes, we can resubscribe.
+func (c *Client) Subscribe(sub event.Subscribe) error {
+	if err := c.Send(sub); err != nil {
+		return err
 	}
 
-	c.Subs.Add(sub)
-	if c.Err = c.Send(sub); c.Err != nil {
-		c.Subs.Remove(sub)
-		return c
-	}
-
-	return c
+	c.AddSub(sub)
+	return nil
 }
 
 // Send takes payload in form of interface and sends it to api
 func (c *Client) Send(pld interface{}) error {
-	if c.Err != nil {
-		return nil
+	b, err := json.Marshal(pld)
+	if err != nil {
+		return err
 	}
-	b, _ := json.Marshal(pld)
-	return wsutil.WriteClientBinary(c.Conn, b)
+
+	return wsutil.WriteClientBinary(c.conn, b)
 }
 
+// Read starts consuming data stream
 func (c *Client) Read(ch chan<- msg.Msg) {
-	defer c.Conn.Close()
+	defer c.conn.Close()
 
 	for {
-		ms, opCode, err := wsutil.ReadServerData(c.Conn)
-		m := msg.Msg{Data: ms, CID: c.ID}
+		ms, opCode, err := wsutil.ReadServerData(c.conn)
+		m := msg.Msg{Data: ms, CID: c.id}
 
 		if err != nil {
 			m.Err = err
@@ -123,4 +127,37 @@ func (c *Client) Read(ch chan<- msg.Msg) {
 
 		ch <- m
 	}
+}
+
+// SubsLimitReached returns true if number of subs > subsLimit
+func (c *Client) SubsLimitReached() bool {
+	if c.subsLimit == 0 {
+		return false
+	}
+	return len(c.subs) == c.subsLimit
+}
+
+// SubAdded checks if given subscription is already added. Used to
+// avoid duplicate subscriptions per client
+func (c *Client) SubAdded(sub event.Subscribe) (isAdded bool) {
+	_, isAdded = c.subs[sub]
+	return
+}
+
+// AddSub adds new subscription to the list
+func (c *Client) AddSub(sub event.Subscribe) {
+	c.subs[sub] = true
+}
+
+// RemoveSub adds new subscription to the list
+func (c *Client) RemoveSub(sub event.Subscribe) {
+	delete(c.subs, sub)
+}
+
+// GetAllSubs returns all subscriptions
+func (c *Client) GetAllSubs() (res []event.Subscribe) {
+	for sub := range c.subs {
+		res = append(res, sub)
+	}
+	return
 }
