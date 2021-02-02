54839a2c61206f17ba34c84d624ef35b0a119391
Daniel1984 <aciddaniel@gmail.com>
2021-02-02 15:20:44 +0200

    updating margin info model with missing fields

--- pkg/models/margin/margin.go
+++ pkg/models/margin/margin.go
@@ -11,11 +11,15 @@ type InfoBase struct {
 	UserSwaps      float64
 	MarginBalance  float64
 	MarginNet      float64
+	MarginRequired float64
 }
 
 type InfoUpdate struct {
 	Symbol          string
 	TradableBalance float64
+	GrossBalance    float64
+	Buy             float64
+	Sell            float64
 }
 
 // FromRaw returns either a InfoBase or InfoUpdate, since
@@ -30,14 +34,16 @@ func FromRaw(raw []interface{}) (o interface{}, err error) {
 		return o, fmt.Errorf("expected margin info type in first position for margin info but got %#v", raw)
 	}
 
-	if len(raw) == 2 && typ == "base" { // This should be ["base", [...]]
+	if len(raw) > 1 && typ == "base" { // This should be ["base", [...]]
 		data, ok := raw[1].([]interface{})
 		if !ok {
 			return o, fmt.Errorf("expected margin info array in second position for margin info but got %#v", raw)
 		}
 
 		return baseFromRaw(data)
-	} else if len(raw) == 3 && typ == "sym" { // This should be ["sym", SYMBOL, [...]]
+	}
+
+	if len(raw) > 2 && typ == "sym" { // This should be ["sym", SYMBOL, [...]]
 		symbol, ok := raw[1].(string)
 		if !ok {
 			return o, fmt.Errorf("expected margin info symbol in second position for margin info update but got %#v", raw)
@@ -55,28 +61,32 @@ func FromRaw(raw []interface{}) (o interface{}, err error) {
 }
 
 func updateFromRaw(symbol string, raw []interface{}) (o *InfoUpdate, err error) {
-	if len(raw) < 1 {
+	if len(raw) < 4 {
 		return o, fmt.Errorf("data slice too short for margin info update: %#v", raw)
 	}
 
 	o = &InfoUpdate{
 		Symbol:          symbol,
 		TradableBalance: convert.F64ValOrZero(raw[0]),
+		GrossBalance:    convert.F64ValOrZero(raw[1]),
+		Buy:             convert.F64ValOrZero(raw[2]),
+		Sell:            convert.F64ValOrZero(raw[3]),
 	}
 
 	return
 }
 
-func baseFromRaw(raw []interface{}) (o *InfoBase, err error) {
-	if len(raw) < 4 {
-		return o, fmt.Errorf("data slice too short for margin info base: %#v", raw)
+func baseFromRaw(raw []interface{}) (ib *InfoBase, err error) {
+	if len(raw) < 5 {
+		return ib, fmt.Errorf("data slice too short for margin info base: %#v", raw)
 	}
 
-	o = &InfoBase{
+	ib = &InfoBase{
 		UserProfitLoss: convert.F64ValOrZero(raw[0]),
 		UserSwaps:      convert.F64ValOrZero(raw[1]),
 		MarginBalance:  convert.F64ValOrZero(raw[2]),
 		MarginNet:      convert.F64ValOrZero(raw[3]),
+		MarginRequired: convert.F64ValOrZero(raw[4]),
 	}
 
 	return
