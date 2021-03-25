411b487e001772582c900b4126dd118ff0c04331
Daniel1984 <aciddaniel@gmail.com>
2021-03-25 13:52:46 +0200

    updating common model values for FIX to consume

--- pkg/models/common/common.go
+++ pkg/models/common/common.go
@@ -6,43 +6,58 @@ import (
 )
 
 const (
-	OrderFlagHidden        int              = 64
-	OrderFlagClose         int              = 512
-	OrderFlagPostOnly      int              = 4096
-	OrderFlagOCO           int              = 16384
-	Checksum               int              = 131072
-	OrderTypeExchangeLimit                  = "EXCHANGE LIMIT"
-	PermissionRead                          = "r"
-	PermissionWrite                         = "w"
-	FundingPrefix                           = "f"
-	TradingPrefix                           = "t"
-	FundingSizeKey         StatKey          = "funding.size"
-	CreditSizeKey          StatKey          = "credits.size"
-	CreditSizeSymKey       StatKey          = "credits.size.sym"
-	PositionSizeKey        StatKey          = "pos.size"
-	Bid                    OrderSide        = 1
-	Ask                    OrderSide        = 2
-	Long                   OrderSide        = 1
-	Short                  OrderSide        = 2
-	OldestFirst            SortOrder        = 1
-	NewestFirst            SortOrder        = -1
-	OneMinute              CandleResolution = "1m"
-	FiveMinutes            CandleResolution = "5m"
-	FifteenMinutes         CandleResolution = "15m"
-	ThirtyMinutes          CandleResolution = "30m"
-	OneHour                CandleResolution = "1h"
-	ThreeHours             CandleResolution = "3h"
-	SixHours               CandleResolution = "6h"
-	TwelveHours            CandleResolution = "12h"
-	OneDay                 CandleResolution = "1D"
-	OneWeek                CandleResolution = "7D"
-	TwoWeeks               CandleResolution = "14D"
-	OneMonth               CandleResolution = "1M"
-	Precision0             BookPrecision    = "P0" // Aggregate precision levels
-	Precision1             BookPrecision    = "P1" // Aggregate precision levels
-	Precision2             BookPrecision    = "P2" // Aggregate precision levels
-	Precision3             BookPrecision    = "P3" // Aggregate precision levels
-	PrecisionRawBook       BookPrecision    = "R0" // Raw precision
+	OrderFlagHidden               int              = 64
+	OrderFlagClose                int              = 512
+	OrderFlagPostOnly             int              = 4096
+	OrderFlagOCO                  int              = 16384
+	Checksum                      int              = 131072
+	OrderStatusActive                              = "ACTIVE"
+	OrderStatusExecuted                            = "EXECUTED"
+	OrderStatusPartiallyFilled                     = "PARTIALLY FILLED"
+	OrderStatusCanceled                            = "CANCELED"
+	OrderTypeExchangeLimit                         = "EXCHANGE LIMIT"
+	OrderTypeMarket                                = "MARKET"
+	OrderTypeExchangeMarket                        = "EXCHANGE MARKET"
+	OrderTypeLimit                                 = "LIMIT"
+	OrderTypeStop                                  = "STOP"
+	OrderTypeExchangeStop                          = "EXCHANGE STOP"
+	OrderTypeTrailingStop                          = "TRAILING STOP"
+	OrderTypeExchangeTrailingStop                  = "EXCHANGE TRAILING STOP"
+	OrderTypeFOK                                   = "FOK"
+	OrderTypeExchangeFOK                           = "EXCHANGE FOK"
+	OrderTypeStopLimit                             = "STOP LIMIT"
+	OrderTypeExchangeStopLimit                     = "EXCHANGE STOP LIMIT"
+	PermissionRead                                 = "r"
+	PermissionWrite                                = "w"
+	FundingPrefix                                  = "f"
+	TradingPrefix                                  = "t"
+	FundingSizeKey                StatKey          = "funding.size"
+	CreditSizeKey                 StatKey          = "credits.size"
+	CreditSizeSymKey              StatKey          = "credits.size.sym"
+	PositionSizeKey               StatKey          = "pos.size"
+	Bid                           OrderSide        = 1
+	Ask                           OrderSide        = 2
+	Long                          OrderSide        = 1
+	Short                         OrderSide        = 2
+	OldestFirst                   SortOrder        = 1
+	NewestFirst                   SortOrder        = -1
+	OneMinute                     CandleResolution = "1m"
+	FiveMinutes                   CandleResolution = "5m"
+	FifteenMinutes                CandleResolution = "15m"
+	ThirtyMinutes                 CandleResolution = "30m"
+	OneHour                       CandleResolution = "1h"
+	ThreeHours                    CandleResolution = "3h"
+	SixHours                      CandleResolution = "6h"
+	TwelveHours                   CandleResolution = "12h"
+	OneDay                        CandleResolution = "1D"
+	OneWeek                       CandleResolution = "7D"
+	TwoWeeks                      CandleResolution = "14D"
+	OneMonth                      CandleResolution = "1M"
+	Precision0                    BookPrecision    = "P0" // Aggregate precision levels
+	Precision1                    BookPrecision    = "P1" // Aggregate precision levels
+	Precision2                    BookPrecision    = "P2" // Aggregate precision levels
+	Precision3                    BookPrecision    = "P3" // Aggregate precision levels
+	PrecisionRawBook              BookPrecision    = "R0" // Raw precision
 	// FrequencyRealtime book frequency gives updates as they occur in real-time.
 	FrequencyRealtime BookFrequency = "F0"
 	// FrequencyTwoPerSecond delivers two book updates per second.
@@ -78,6 +93,8 @@ type Mts int64
 type StatKey string
 
 type StatusType string
+
+type OrderType string
 
 func CandleResolutionFromString(str string) (CandleResolution, error) {
 	switch str {
